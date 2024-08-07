be294d34c70711466392b5144df3a124da739821
Wagner <*wagner.acorreia@senacsp.edu.br>
2024-08-06 22:02:44 -0300

    Aulas

---
+++ A-15/Funcoes/A-15.4.js
@@ -0,0 +1,11 @@
+function saudacao() {
+    let nome = prompt('Digite seu nome')
+    console.log('Seja Bem-Vindo!' + nome);
+    document.write('Seja Bem-Vindo ' + nome);
+}
+
+saudacao();
+
+/* Como criar uma função?
+Para criar uma função, você usa a palavra-chave function, seguida pelo nome da função, parênteses e
+um bloco de código entre chaves { }.*/
